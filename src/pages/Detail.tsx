120be5041fd2dca97177de1ae733d13d6d66918c
Mees Goudbeek <meesgoudbeek@protonmail.com>
2022-04-20 15:43:34 +0200

    added first part of assignment

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -6,7 +6,7 @@ import Information from "../components/Information";
 const Img = styled("img")({
   display: "block",
   width: "100%",
-  maxHeight: "500px",
+  maxHeight: "600px",
   objectFit: "cover",
   objectPosition: "0 20%",
 });
@@ -21,32 +21,25 @@ const Detail = () => {
     <>
       {artworkDetail && (
         <main>
-          <Box
-            sx={{
-              pt: 8,
-              pb: 6,
-            }}
-          >
-            <Container sx={{ py: 8 }} maxWidth="lg">
-              <Grid container spacing={2}>
-                <Grid item xs={12} md={12}>
-                  <Img
-                    alt={artworkDetail.artObject.title}
-                    src={artworkDetail.artObject.webImage.url}
-                  />
-                </Grid>
-                <Grid item xs={12} md={12}>
-                  <Typography variant="h5" component="div" gutterBottom>
-                    {artworkDetail.artObject.title}
-                  </Typography>
-                  <Typography variant="subtitle1">
-                    {artworkDetail.artObject.scLabelLine}
-                  </Typography>
-                </Grid>
+          <Container sx={{ py: 8 }} maxWidth="lg">
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={12}>
+                <Img
+                  alt={artworkDetail.artObject.title}
+                  src={artworkDetail.artObject.webImage.url}
+                />
               </Grid>
-              <Information artworkDetail={artworkDetail} />
-            </Container>
-          </Box>
+              <Grid item xs={12} md={12}>
+                <Typography variant="h5" component="div" gutterBottom>
+                  {artworkDetail.artObject.title}
+                </Typography>
+                <Typography variant="subtitle1">
+                  {artworkDetail.artObject.scLabelLine}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Information artworkDetail={artworkDetail} />
+          </Container>
         </main>
       )}
     </>
